4014abf95bad0c40952d06b5d6efcf3aa9dad3ad
Salvador Cuevas <salvador.x.cuevas.galvez.-nd@disney.com>
2019-11-19 23:10:52 -0600

    first model of application

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: HomeColumn(),
+  home: NinjaCard(),
 ));
 
-class HomeColumn extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Hello there'),
+        title: Text('Pokemon Trainer ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-              child: Image.asset('assets/image-2.jpg'),
-              flex: 1,
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.teal,
-              child: Text('1'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/pokemon3.png'),
+                radius: 40.0,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.pinkAccent,
-              child: Text('2'),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.deepPurpleAccent,
-              child: Text('3'),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Text('click'),
-        backgroundColor: Colors.blue[400],
+            SizedBox(height: 10.0),
+            Text(
+              'Carlos',
+              style: TextStyle(
+                  color: Colors.lightBlueAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'CURRENT TRAINER LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '40',
+              style: TextStyle(
+                  color: Colors.lightBlueAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'carlos@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  )
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
