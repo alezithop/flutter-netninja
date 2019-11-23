a56099d10750c463d2ad12adbc0a676552156232
Salvador Cuevas <salvador.x.cuevas.galvez.-nd@disney.com>
2019-11-23 17:26:56 -0600

    implementing Card widget and adding a function to return the stylized card to the home

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,35 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'Claire Redfield', text: 'Are you a police officer?')
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +60,7 @@ class _QuoteListState extends State<QuoteList> {
        /* children: quotes.map((quote){
           return Text(quote);
         }).toList(),*/
-       children: quotes.map((quote) => Text('${quote.text} by ${quote.author}')).toList(),
+       children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
