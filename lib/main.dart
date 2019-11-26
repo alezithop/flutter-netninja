fd35aef04701a6a0430eb30d0a6129a4c60928f0
Salvador Cuevas <salvador.x.cuevas.galvez.-nd@disney.com>
2019-11-25 21:48:36 -0600

    showing results of the API in screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:myapp/pages/loading.dart';
 
 void main() =>
     runApp(MaterialApp(
-      initialRoute: '/home',
+      initialRoute: '/loading',
       routes: {
         '/': (context) => Loading(),
         '/home': (context) => Home(),
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:myapp/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,51 +8,31 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  String time = 'Loading';
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    // showing data received in response.
-    print(response.body);
+  void setupWorldTime() async {
 
-    // converting data with jsonDecode and adding it to a new type Map variable
-    // we are going to be able to access to the properties that the response has.
-    // we need to import "dart:convert" to use jsonDecode
-    Map data = jsonDecode(response.body);
-    // showing processed data
-    print(data);
-    // showing only one properties of the processed data.
-    print(data['title']);
-  }
-
-  void getTime() async {
-    // make the request
-    Response response = await get('http://worldtimeapi.org/api/timezone/America/Monterrey');
-    Map data = jsonDecode(response.body);
-    // print(data);
-
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-
-    print(datetime);
-    print(offset);
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+    WorldTime wt = WorldTime(location: 'Monterrey', flag: 'mexico.png', url: 'America/Monterrey');
+    await wt.getTime();
+    print(wt.time);
+    setState(() {
+      time = wt.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
